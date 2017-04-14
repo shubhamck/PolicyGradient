ad97404a46edc8c55c3556ab89d7237a7a656b87
srikanthmalla <mallasrikanth004@gmail.com>
2017-04-14 17:28:55 -0400

    advantage , biasline is found

--- policyGradient.py
+++ policyGradient.py
@@ -17,6 +17,9 @@ if __name__=="__main__":
 	#Initialize Actor
 	actor = actor.Actor(4,2)
 	actor.createModel()	
+	#Initialize Critic
+	critic = critic.Critic(4)
+	critic.createModel()	
 
 	
 	#for n policies 		
@@ -29,16 +32,26 @@ if __name__=="__main__":
 			action_prob = actor.act(observation)
 			action = np.argmax(action_prob)
 			observation, reward, done, info = env.step(action)
+			observation = np.reshape(observation,(1,4))
 			T.append([observation,action,reward])
 			if done:
 				break
-		print T
+#		print T
 		
 		#Got trajectory
 		# Get Rt
 	
 		T[len(T)-1].append(T[len(T)-1][2])		
-		for i in range(len(T)-2, 0, -1):
+		for i in range(len(T)-2, -1, -1):
 			T[i].append(T[i][2]+GAMMA*T[i+1][3])
-		print T
+#		print T		
+		# find bt which is value
+		for i in range(len(T)):
+			T[i].append(critic.Value(T[i][0]))
+		#find A which is Advantage (R-b)
+		for i in range(len(T)):
+			T[i].append(T[i][-2]-T[i][-1])
+		print T[1]
+	
+
 		
